ab9ddb365438b5c8af53aeb61b4358dbe08e4c39
Brian Rosner <brian@brosner.com>
2016-08-13 11:18:41 -0600

    Bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-gapc-storage",
-    version="0.1.0",
+    version="0.2.0",
     author="Eldarion, Inc.",
     author_email="development@eldarion.com",
     description="a Django storage backend using GCS JSON API",
